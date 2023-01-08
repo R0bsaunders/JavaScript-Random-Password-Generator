29735075fca86299965a7513afe96affa057375e
Rob S <me@rob-saunders.co.uk>
2023-01-08 23:43:04 +0000

    Updated Readme file and made error alerts more informative for the user

--- assets/README.md
+++ assets/README.md
@@ -1,2 +1,46 @@
 # JavaScript-Random-Password-Generator
-Use this little tool to generate a password between 10-64 characters long with multiple character options
+
+## Description
+
+This is a demonstration of my newly learn JavaScript functions and objects to create a tool that can generate a random password between 10-64 characters long with multiple character options. The criteria is as follows:
+
+* Generate a password when the button is clicked.
+* Present a series of prompts for password criteria:
+    * Length of password:
+    * At least 10 characters but no more than 64.
+    * Character types:
+    * Lowercase
+    * Uppercase
+    * Numeric
+    * Special characters ($@%&*, etc.)
+    * Code should validate for each input and at least one character type should be selected.
+    * Once all prompts are answered, the password should be generated and displayed in an alert or written to the page.
+
+The finished application should like the below:
+
+![javascript-challenge-demo](/assets/images/05-javascript-challenge-demo.png)
+
+
+## Usage
+
+The gitHub Repository is here: https://github.com/R0bsaunders/JavaScript-Random-Password-Generator
+The live working version is here: https://r0bsaunders.github.io/JavaScript-Random-Password-Generator/
+
+All JavaScript can be found here: ![JavaScript](/assets/script/script.js)
+It will look like this: ![password-generator-screenshot](/assets/images/password-generator-screenshot.jpg)
+
+Note: There was an array of letters in both uppercase and lowercase provided in the starter code. However, we have learnt about some methods, and I have used .toUpperCase() so that only an array of lowercase letters need to be declared. I have removed the array of uppercase characters as they are not necessary now. If the user chooses to include uppercase letters then the toUpperCase() function store the letters in the available characters array as uppercase.
+
+Please note the following conditionals:
+
+* On choosing the length of the password, the code validates the user's input and prompts a message (relevant to their input) in the following senarious:
+    * If the input is not numeric i.e. letters & special characters
+    * The number is less than 'min' (stored as a variable incase it ever needs to change). Currently set to 10 for this exercise
+    * The number is more than 'max' (stored as a variable incase it ever needs to change). Currently set to 64 for this exercise
+    * If the user does not enter anything but clicks OK.
+
+* If the user does not select any options, then they are alerted and told to select at least one option or cancel
+
+* If the user wants to cancel, they are taken out of the loop.
+
+It was really important to me that the messaging was intuitive as well. Guiding the user along the way. I also included any erroneous values the user entered, and explained why there is an error and remedial action. 
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,94 +1,11 @@
 // Array of special characters to be included in password
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
 
 // Array of numeric characters to be included in password
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-// Array of lowercase characters to be included in password
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
-];
-
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
-
-
+// Array of letters to be included in password
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 const min = 10;
 const max = 64;
@@ -109,19 +26,29 @@ function getPasswordOptions() {
     securePwd = '';
     pwdArray = '';
     
-    pwdLength = prompt(`Please enter your desired password length between ${min} - ${max}`)
+    pwdLength = prompt(`Please enter your desired password length between ${min} - ${max} and click OK.\nClick Cancel to exit`)
+
+    // Conditional: If user cancels
 
     if (pwdLength === null) {
         return;
 
+    // Conditional: If nothing is entered
+
     } else if (pwdLength === "") {
         alert(`You haven't typed anything! You must enter a number between ${min} - ${max}`)
-      
+
+    // Conditional: If a letter or character is entered
+
     } else if (pwdLength != parseInt(pwdLength)) {
-        alert(`You've entered letters, you must enter a number between ${min} - ${max}`)
+        alert(`'${pwdLength}' is not a number! You've entered letters; you must enter a number between ${min} - ${max}`)
+
+    // Conditional: If the number is too high
 
     } else if (pwdLength > max) {
         alert(`${pwdLength} is higher than ${max}. Choose a number between ${min} and ${max}`)
+
+    // Conditional: If the number is too low
 
     } else if (pwdLength < min) {
         alert(`${pwdLength} is less than ${min}. Choose a number between ${min} and ${max}`)
@@ -132,36 +59,37 @@ function getPasswordOptions() {
     };
 
   } while (true);
-
-  // Debug: View user's chosen password length
-  console.log("Password length is " + pwdLength);
-  
+ 
+  // The following do loop is for presenting user the character options.
+  // After each selection, the array of available characters is built
   
   do {
 
-    userChoices.lowerCase = confirm("Do you want your password to include lowercase letters?");
+    userChoices.lowerCase = confirm("Do you want your password to include lowercase letters?\nClick 'OK' for YES\nClick 'Cancel' for NO");
       if (userChoices.lowerCase) {
-        pwdArray += lowerCasedCharacters.join("");
+        pwdArray += letters.join("");
       };
 
-    userChoices.numbers = confirm("Do you want your password to include numbers?");
+    userChoices.numbers = confirm("Do you want your password to include numbers?\nClick 'OK' for YES\nClick 'Cancel' for NO");
       if (userChoices.numbers) {
         pwdArray += numericCharacters.join("");
       };
 
-    userChoices.specialChars = confirm("Do you want your password to include special Characters?");
+    userChoices.specialChars = confirm("Do you want your password to include special Characters?\nClick 'OK' for YES\nClick 'Cancel' for NO");
       if (userChoices.specialChars) {
         pwdArray += specialCharacters.join("");
       };
 
-    userChoices.upperCase = confirm("Do you want your password to include uppercase letters?");
+    userChoices.upperCase = confirm("Do you want your password to include uppercase letters?\nClick 'OK' for YES\nClick 'Cancel' for NO");
       if (userChoices.upperCase) {
-        pwdArray += upperCasedCharacters.join("");
+        pwdArray += letters.join("").toUpperCase();
       };
 
     if (userChoices.lowerCase === false && userChoices.numbers === false && userChoices.specialChars === false && userChoices.upperCase === false) {
 
-      if (!confirm("You must make at least one selection")) {
+      // This confirm box needs to return to the top of the do loop if the user clicks OK. As this returns a 'true' value, this needs to be switched to false using the !
+
+      if (!confirm("You must make choose at least one option. Do you want to choose again or cancel?\nClick 'OK' for YES\nClick 'Cancel' for NO")) {
         return;
 
       };
@@ -173,15 +101,10 @@ function getPasswordOptions() {
 
   } while (true);
 
-  // Debug: View user's option states
-  console.log("User selected the following options: " + JSON.stringify(userChoices));
-  console.log("Available characters for password are: " + pwdArray);
-
+  // The array containing all options the user selected is now fed into the getRandom function which generates a new array using random characters to the length specified by the user
+  
   getRandom(pwdArray);
-
-  // Debug: View the password in console
-  console.log("The password is: " + securePwd);
-
+  
 };
 
 // Function for getting a random element from an array
@@ -190,7 +113,8 @@ function getRandom(arr) {
   for (let i = 0; i < pwdLength; i++) {
     securePwd += arr[Math.floor(Math.random() * arr.length)];
 
-  }
+  };
+
 }
 
 // Function to generate password with user input
@@ -205,7 +129,7 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
   var passwordText = document.querySelector('#password');
 
   passwordText.value = securePwd;
